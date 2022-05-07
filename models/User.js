3beb53abbcdee32d415efd396448a7fbb119c046
Brooke Love <savienloveb@gmail.com>
2022-05-07 14:01:14 -0700

    creating username and password model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const {Model, DataTypes} = require ('sequelize');
+const sequelize = require ('../config/connection');
+const bcrypt = require("bcrypt");
+
+
+class User extends Model {}
+
+User.init ({
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unqiue: true 
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+            len:[8]
+        }
+    }
+}, {
+    hooks: {
+        beforeCreate: async userdata => {
+            userdata.password = await bcrypt.hash(userdata.password, 5)
+            return userdata
+        }
+    },
+    sequelize,
+})
